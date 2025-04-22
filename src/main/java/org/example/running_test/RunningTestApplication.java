0f29c3d96150ec323f0b801f81fc7b2d0ec55714
lsheon0927@naver.com <lsheon0927@naver.com>
2025-04-22 13:40:32 +0900

    "Initialize SpringBoot project"

---
+++ src/main/java/org/example/running_test/RunningTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.running_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/running_test/RunningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.running_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
